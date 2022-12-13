b532cb9573e4247fa31d29ab06b060ea8d8f3914
Ramlov <mathias.ramlov10@gmail.com>
2022-12-13 15:29:28 +0100

    Simple script to buy crypto

---
+++ Trading Bot/Trading Bot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using CoinbasePro.Network.Authentication;
+using CoinbasePro.Network.HttpClient;
+using CoinbasePro.Services.Orders;
+using CoinbasePro.Services.Orders.Models.Responses;
+
+namespace CryptoBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Replace with your API key and secret
+            var apiKey = "";
+            var apiSecret = "";
+
+            // Authenticate with Coinbase Pro
+            var credentials = new ApiCredentials(apiKey, apiSecret);
+            var client = new CoinbaseProHttpClient(credentials);
+            var ordersService = new OrdersService(client);
+
+            // Set up the parameters for the trade
+            var productId = "BTC-USD";
+            var side = "buy";
+            var orderType = "market";
+            var funds = 0.1m;
+
+            // Execute the trade
+            var response = ordersService.PlaceOrder(productId, side, orderType, funds);
+
+            // Print the response
+            Console.WriteLine("Trade complete: " + response.Id);
+        }
+    }
+}
